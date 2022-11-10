f637c39eed438e37ec1ec977477aaebf1f2b8cb8
Bahadır <m.bahadirtasli@gmail.com>
2022-11-10 03:29:10 +0300

    List-exercise added.

--- src/main/java/org/sda/Main.java
+++ src/main/java/org/sda/Main.java
@@ -1,120 +1,123 @@
 package org.sda;
 
-import org.sda.model.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-import java.util.Arrays;
-
+/**
+ * Create a List and display its result (data should be provided by the user - console):
+ * a) Purchases to be made. *If an element already exists on the list, then it should
+ * not be added.
+ * b) *Add to the example above the possibility of "deleting" purchased elements
+ * c) Display only those purchases that start with „m” (e.g. milk)
+ * d) * View only purchases whose next product on the list starts with „m” (e.g. eggs,
+ * if milk was next on the list)
+ */
 public class Main {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
-        //ENCAPSULATION
-        //No-arg constructor call
-        Person person = new Person();
-        System.out.println(person.toString());
-        System.out.println(person.getId());
+        System.out.println("Welcome to the shop");
 
-        //All-argument constructor call
-        Person person1 = new Person(12345L, "Vinod", "John", "abc@gmail.com", "+37258964253", "Tartu tee, Tallinn");
-        System.out.println(person1.toString());
-        System.out.println(person1.getEmail());
+        List<String> shoppingBag = new ArrayList<>();
+        menuOperation(shoppingBag);
+    }
 
-        Person person2 = new Person();
-        person2.setFirstName("Tony");
-        person2.setLastName("Stark");
-        System.out.println(person2.getFirstName());
-        System.out.println(person2.getLastName());
+    private static void menuOperation(List<String> shoppingBag) {
+        int menuOption = getMenu();
 
+        switch (menuOption) {
+            case 1: // Add item
+                addItem(shoppingBag);
+                menuOperation(shoppingBag);
+                break;
+            case 2: // Delete item
+                deleteItem(shoppingBag);
+                menuOperation(shoppingBag);
+                break;
+            case 3: // Display items
+                displayItems(shoppingBag);
+                menuOperation(shoppingBag);
+                break;
+            case 4: // exit
+                System.out.println("Thanks for shopping. Visit us again!");
+                break;
+            default:
+                System.out.println("Incorrect option, choose the correct one!");
+                menuOperation(shoppingBag);
+        }
+    }
 
-        Dog dog = new Dog(true, "GERMAN SHEPPERD");
-        System.out.println(dog.toString());
-        System.out.println(dog.getAge());
-        System.out.println(dog.getWeight());
+    private static int getMenu() {
+        System.out.println("--------------");
+        System.out.println("     MENU    ");
+        System.out.println("--------------");
 
+        List<String> menu = List.of("Add item", "Delete item", "Display items", "Exit");
 
-        //INHERITANCE
-        Passenger passenger = new Passenger(PaymentType.CARD, "Parnu");
-        passenger.setEmail("passenger@gmail.com"); // Access Person fields using Passenger object
-
-        PrivatePassenger privatePassenger = new PrivatePassenger(PaymentType.CARD, "Viljandi");
-        privatePassenger.setPersonalIDCode("238746521873"); //Access PrivatePassenger's field
-        privatePassenger.setDestinationAddress("tartu"); //Access Passenger's field
-        privatePassenger.setPhoneNumber("+3722387647654"); //Access Person's field
-
-
-        //Overriding
-        Person personOverride = new Person();
-        personOverride.setEmail("vinodjohn@gmail.com");
-        System.out.println(personOverride.getEmail());
-
-        Passenger passengerOverride = new Passenger();
-        passengerOverride.setEmail("vinodjohn@gmail.com");
-        System.out.println(passengerOverride.getEmail());
-
-
-        //Polymorphism
-        Person person3 = new Person(12345L, "Parnu");
-        Person person4 = new Passenger(PaymentType.CARD, "Viljandi");
-        System.out.println(person3.toString());
-        System.out.println(person4.toString());
-
-
-        // Calling parent methods
-        Passenger passenger1 = new Passenger();
-        passenger1.setAddress("Tallinn"); // Person.address
-        passenger1.setDestinationAddress("Tartu"); // Passenger.destinationAddress
-        System.out.println(passenger1.getAddresses());
-
-
-        //Calling parent's hidden field
-        passenger1.getHiddenAlive();
-
-        //passing parameters
-        Passenger passenger2 = new Passenger(123456L, "Tallinn", PaymentType.CASH, "Tartu");
-        printPersonAddress(passenger2);
-        printPassengerWithPrefix("Passenger: ", passenger2);
-
-
-        //Composition exercise
-        Muzzle muzzle = new Muzzle();
-        muzzle.setId(890L);
-        muzzle.setNoiseRange(10);
-        muzzle.setTooNoisy(true);
-
-
-        Dog dog1 = new Dog(true, "DOBER");
-        dog1.setMuzzle(muzzle);
-
-        System.out.println(dog1.getMuzzle().toString());
-
-
-        //enums
-        System.out.println(PaymentType.CARD); // prints enum 'CARD'
-        System.out.println(Arrays.toString(PaymentType.values())); // prints all the enum values
-        System.out.println(PaymentType.BANK_TRANSFER.getValue()); // prints the value of the enum '3'
-
-        // looping enums
-        for(PaymentType paymentType: PaymentType.values()) {
-            System.out.println(paymentType.name());
+        for (int i = 0; i < menu.size(); i++) {
+            System.out.println(i + 1 + ". " + menu.get(i));
         }
 
-        // enum method overriding
-        System.out.println(PaymentType.MOBILE_BANKING.toString());
+        System.out.println("Choose a menu from above:");
 
-
-        //Enum exercise
-        System.out.println(">>>>>>>>>>>>>PLANETS<<<<<<<<<<<<<<<<");
-        System.out.println(Planets.JUPITER.toString());
-        System.out.println("Distance of " + Planets.JUPITER.name() + " from Earth: " + Planets.JUPITER.distanceFromEarth());
-        System.out.println("-------------------");
-        System.out.println(Planets.MARS.toString());
-        System.out.println("Distance of " + Planets.MARS.name() + " from Earth: " + Planets.MARS.distanceFromEarth());
+        return SCANNER.nextInt();
     }
 
+    private static void addItem(List<String> shoppingBag) {
+        boolean isAdd = true;
 
-    private static void printPersonAddress(Person person) {
-        System.out.println(person.getAddress());
+        while (isAdd) {
+            System.out.println("Enter an item name to be added to the bag: ");
+            String addItem = SCANNER.next();
+
+            if (!shoppingBag.contains(addItem)) {
+                shoppingBag.add(addItem);
+                System.out.println("'" + addItem + "' added to the bag. Do you want to add more item? (true/false)");
+            } else {
+                System.out.println("The item already exists, do you want to add an other item? (true/false)");
+            }
+
+            isAdd = SCANNER.nextBoolean();
+        }
     }
 
-    private static void printPassengerWithPrefix(String prefix, Object object) {
-        System.out.println(prefix + object);
+    private static void deleteItem(List<String> shoppingBag) {
+        boolean isDelete = true;
+
+        while (isDelete) {
+            System.out.println("Enter an item name to be deleted from the bag: ");
+            String deleteItem = SCANNER.next();
+
+            if (shoppingBag.contains(deleteItem)) {
+                shoppingBag.remove(deleteItem);
+                System.out.println("'" + deleteItem + "' deleted from the bag. Do you want to delete more item? (true/false)");
+            } else {
+                System.out.println("The item not exists, do you want to delete an other item? (true/false)");
+            }
+
+            isDelete = SCANNER.nextBoolean();
+        }
+    }
+
+    private static void displayItems(List<String> shoppingBag) {
+        int counter = 1;
+
+        System.out.println("ITEMS: ");
+
+        for (int i = 0; i < shoppingBag.size(); i++) {
+            String thisItem = shoppingBag.get(i);
+            String nextItem = (i + 1) <= (shoppingBag.size() - 1) ? shoppingBag.get(i + 1) : "";
+
+            if (thisItem.startsWith("m") || thisItem.startsWith("M") || nextItem.startsWith("m") || nextItem.startsWith("M")) {
+                System.out.println(counter + ". " + thisItem);
+                counter++;
+            }
+        }
+
+
+        if (counter == 1) {
+            System.out.println("Your shopping bag is empty or doesn't contain the expected items. No items to display!");
+        }
     }
 }
